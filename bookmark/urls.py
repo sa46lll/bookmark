b3c93e909d16dd6ef110b59cb4790f8334e85b5f
sa46lll <9812smh@naver.com>
2021-09-02 21:30:25 +0900

    [Feat] 북마크 확인, 수정 기능 구현

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -5,4 +5,6 @@ from bookmark.views import *
 urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'),
     path('add/', BookmarkCreateView.as_view(), name='add'),
+    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', BookmarkUpdateView.as_view(), name='update'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -9,11 +9,22 @@ from bookmark.models import Bookmark
 
 class BookmarkListView(generic.ListView):
     model = Bookmark
+    # 템플릿 속성이 없을때, '객체이름_list.html' 템플릿을 보여줌.
 
 
 class BookmarkCreateView(generic.CreateView):
     model = Bookmark
     fields = ['site_name', 'url']
-    success_url = reverse_lazy('list') # 글쓰기를 완료하고 이동할 목록페이지
+    success_url = reverse_lazy('list')  # 글쓰기를 완료하고 이동할 목록페이지
     # reverse_lazy: 나중에 해당 변수가 직접 호출되었을때 evalute 함.
-    template_name_suffix = '_create'
+    template_name_suffix = '_create'  # bookmark_craete.html 템플릿 파일 호출
+
+
+class BookmarkDetailView(generic.DetailView):
+    model = Bookmark
+
+
+class BookmarkUpdateView(generic.UpdateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    template_name_suffix = '_update'
